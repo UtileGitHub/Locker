5e53d0016d3f48c6bf705d9fc3d119b8a55f6e53
Kjell Irgens <hextremist@gmail.com>
2019-05-08 07:01:06 +0200

    Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app class="info white--text" flat>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Locker DUDE</span>
+        <span class="font-weight-light"> SUPER EPIC GAMER DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="white--text mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+    <v-footer height="auto" color="success"> <v-layout> <v-flex ys12>
+      Epic
+      <v-spacer></v-spacer>
+      <v-btn fab color="info">
+          <v-icon>add_to_queue</v-icon>
+      </v-btn> </v-flex> </v-layout> 
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
